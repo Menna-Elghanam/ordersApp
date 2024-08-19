13180bb5add65de61365b8c24bc6c456ea6a07df
Menna-Elghanam <mennaelghanam90@gmail.com>
2024-08-19 10:09:32 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Button, ButtonGroup } from "@nextui-org/button";
+
+function App() {
+  return (
+    <div className="text-3xl font-bold underline">
+      <Button color="primary">Button</Button>
+    </div>
+  );
+}
+
+export default App;
